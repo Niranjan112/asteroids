67ec8074e0ae5ca916870ae5a74dd971378fbe72
Niranjan <nsniranjansharma7@gmail.com>
2020-11-06 12:27:23 +0530

    Firestore and auth integration done

---
+++ plugins/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA7jffY5kq9E2tDuAv7gYdC5siu5KwzsSQ',
+  authDomain: 'asteroids-be0f9.firebaseapp.com',
+  databaseURL: 'https://asteroids-be0f9.firebaseio.com',
+  projectId: 'asteroids-be0f9',
+  storageBucket: 'asteroids-be0f9.appspot.com',
+  messagingSenderId: '684682696560',
+  appId: '1:684682696560:web:1a4340de192e1b66b021b5',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+
+export default firebase
